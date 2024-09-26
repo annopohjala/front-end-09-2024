dbc5400cb26d7aed5955eb0631ad78ef23a64d84
annopohjala <anno.pohjala@tptlive.ee>
2024-09-26 09:00:49 +0300

    1130
    1130

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Link to= "">
+      <img className="pilt" src="https://p.ohtuleht.ee/40/i/2019/5/6/nshi2psc.vgz.jpg" alt="Nobe auto" />
+      </Link>
+      
+      <Link to="ostukorv">
+        <button className="nupp">Ostukorvi</button>
+      </Link>
+      <Link to="osta-kinkekaart">
+        <button className="nupp">Kinkekaardid</button>
+      </Link>
+      <Link to="seaded">
+        <button className="nupp">Seaded</button>
+      </Link>
+      <Link to="lisa-toode">
+        <button className="nupp">Lisa toode</button>
+      </Link>
+      <Link to="esindused">
+        <button className="nupp">Esindused</button>
+      </Link>
+      {/* <Link to="">
+        <button className="nupp">Nupp</button>
+      </Link> */}
+
+
+      <Routes>
+        <Route path="" element={<div>Olen avalehel</div>} />
+        <Route path="ostukorv" element={<div>Ostukorv</div>} />
+        <Route path="osta-kinkekaart" element={<div>Kinkekaardid</div>} />
+        <Route path="seaded" element={<div>Seaded</div>} />
+        <Route path="lisa-toode" element={<div>Lisa toode</div>} />
+        <Route path="esindused" element={<div>Esindused</div>} />
+      </Routes>
+    
     </div>
   );
 }
--- veebipood/src/index.js
+++ veebipood/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from "react-router-dom";
+
+// Navigeerimiseks (URLi ja sisu vahetuseks):
+// 1. npm install react-router-dom --> lisab vajalikud koodifailid 
+//                                    internetist node_modules kausta
+// 2. import {BrowserRouter}    --> võtab node_modules kaustast, react-router-dom seest
+//                          BrowserRouteriga seotud loogika, mis võimaldab navigeerida
+// 3. <BrowserRouter><App/>   --> ümbritseme App elemendi, et selles failis oleks võimalik
+//                                    navigeerida
+// 4. App.js failis teeme URLi ja failide vahelisi seoseid
+//     <Route path="/osta-kinkekaart" element={<div>Kinkekaart juba olemas?</div>} />
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
